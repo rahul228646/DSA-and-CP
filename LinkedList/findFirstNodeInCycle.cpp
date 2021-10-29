6ef953df1308d4411a27a8728a9c633136e58f80
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-10-29 21:25:31 +0530

    Create findFirstNodeInCycle.cpp

---
+++ LinkedList/findFirstNodeInCycle.cpp
@@ -0,0 +1,35 @@
+
+Node* findFirstNode(Node* head)
+{
+    // If list is empty or has only one node
+    // without loop
+    if (head == NULL || head->next == NULL)
+        return NULL;
+ 
+    Node *slow = head, *fast = head;
+ 
+
+    slow = slow->next;
+    fast = fast->next->next;
+//     floyd's cycle detection
+    while (fast && fast->next) {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+ 
+    // If loop does not exist
+    if (slow != fast)
+        return NULL;
+ 
+    // If loop exists. Start slow from
+    // head and fast from meeting point.
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+ 
+    return slow;
+}
