cc0d11be73eeb2017f6bb4ccb5c7321eea57ea9f
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-10-29 21:29:32 +0530

    Update FloydCycleDetection.cpp

--- LinkedList/FloydCycleDetection.cpp
+++ LinkedList/FloydCycleDetection.cpp
@@ -1,11 +1,13 @@
 
-https://www.geeksforgeeks.org/how-does-floyds-slow-and-fast-pointers-approach-work/
+// https://www.geeksforgeeks.org/how-does-floyds-slow-and-fast-pointers-approach-work/
 
-1) When slow pointer enters the loop, the fast pointer must be inside the loop. Let fast pointer be distance k from slow.
-2) Now if consider movements of slow and fast pointers, we can notice that distance between them (from slow to fast) 
-   increase by one after every iteration. After one iteration (of slow = next of slow and fast = next of next of fast), 
-   distance between slow and fast becomes k+1, after two iterations, k+2, and so on. When distance becomes n, 
-   they meet because they are moving in a cycle of length n.
+// Time - O(N) Space - O(1)
+
+// 1) When slow pointer enters the loop, the fast pointer must be inside the loop. Let fast pointer be distance k from slow.
+// 2) Now if consider movements of slow and fast pointers, we can notice that distance between them (from slow to fast) 
+//    increase by one after every iteration. After one iteration (of slow = next of slow and fast = next of next of fast), 
+//    distance between slow and fast becomes k+1, after two iterations, k+2, and so on. When distance becomes n, 
+//    they meet because they are moving in a cycle of length n.
 
     bool detectLoop(Node* head)
     {
