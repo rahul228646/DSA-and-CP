9113cbde67b509b16d92bc5a9a3e8bf43a556823
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-10-31 15:50:32 +0530

    Create flatteningLL.cpp

---
+++ LinkedList/flatteningLL.cpp
@@ -0,0 +1,32 @@
+
+// https://www.geeksforgeeks.org/flattening-a-linked-list/
+// Time : Θ(N ^ (2*K))
+
+Node *merge(Node *a, Node *b)
+{
+   struct Node * dummy = new Node(0);
+   struct Node *ptr = dummy;
+   while(a && b) {
+       if(a->data <= b->data) {
+           ptr->bottom = a;
+           a = a->bottom;
+       }
+       else {
+           ptr->bottom = b; 
+           b = b->bottom;
+       }
+       ptr = ptr->bottom;
+   }
+   if(a) ptr->bottom = a;
+   else ptr->bottom = b;
+   return dummy->bottom;
+}
+
+Node *flatten(Node *root)
+{
+   if(!root || !root->next) return root;
+   root->next = flatten(root->next);
+   root = merge(root, root->next);
+   return root;
+}
+
