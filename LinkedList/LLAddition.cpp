68e266efad60cc4456b6136bfcc8f47079405f4e
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-11-01 11:15:47 +0530

    Update LLAddition.cpp

--- LinkedList/LLAddition.cpp
+++ LinkedList/LLAddition.cpp
@@ -49,3 +49,52 @@
         h = reverseLL(h);
         return h;
     }
+
+// for o(1) space
+
+// 1) Find the middle point using tortoise and hare method.
+// 2) Split the linked list into two halves using found middle point in step 1.
+// 3) Reverse the second half.
+// 4) Do alternate merge of first and second halves.
+  
+Node *rev(Node * h) {
+    if(!h || !h->next) return h;
+    Node *x = rev(h->next);
+    h->next->next = h;
+    h->next = nullptr;
+    return x;
+}
+
+Node *middle(Node *h) {
+    Node *s=h, *f=h->next;
+    while(s && f && f->next) {
+        s=s->next;
+        f=f->next->next;
+    }
+    return s;
+}
+Node *merge(Node *a, Node *b) {
+
+    Node *h = a, *t = a;
+    while(a||b) {
+        if(b) {
+            t->next = b;
+            t = t->next;
+            b = b->next;
+        }
+        if(a) {
+            t->next = a;
+            t = t->next;
+            a = a->next;
+        }
+    }
+    return h;
+}
+void reorderList(Node* h) {
+    Node *mid = middle(h);
+    Node *a = h, *b = mid->next;
+    mid->next = nullptr;
+    b = rev(b);
+    h = merge(a, b);
+    
+}
