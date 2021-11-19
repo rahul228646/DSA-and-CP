1874837cd7af621b8f48f23027eb708d6b04626f
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-11-19 13:05:18 +0530

    Update Hamming_dis.cpp

--- BitMask/Hamming_dis.cpp
+++ BitMask/Hamming_dis.cpp
@@ -1,4 +1,7 @@
 
+
+// Time : O(n) Space : O(1)
+
 class Solution {
   public:
   int hammingDistance(int x, int y){
@@ -14,6 +17,25 @@ class Solution {
 };
 
 or
+ 
+// Time : O(k) where k is bits that are different Space : O(1)
+  
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int Xor = x ^ y, ans = 0;
+        while(Xor) {
+            ans++;
+            Xor = (Xor & Xor-1); // this step unsets the right most digit
+        }
+        return ans;
+    }
+};
+
+
+or
+  
+// Time : O(n)  Space : O(1)
   
 class Solution {
 public:
@@ -21,6 +43,21 @@ public:
         int ans = 0;
         for(int i = 0; i < 32; i++)
             ans += (x >> i & 1) != (y >> i & 1);
+        return ans;
+    }
+};
+
+or
+ 
+// Time : O(n)  Space : O(n)
+  
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        bitset<32> xb(x), yb(y);
+        int ans = 0;
+        for(int i = 0; i < 32; i++)
+            ans += xb[i] != yb[i];
         return ans;
     }
 };
