9bae4cc57ae63abb25ab1b61c360c67981febcb8
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2021-11-28 14:26:43 +0530

    Create path_source_to_end(DAG).cpp

---
+++ Graphs/path_source_to_end(DAG).cpp
@@ -0,0 +1,25 @@
+//DAG = Directed Acyclic Graph
+
+
+class Solution {
+public:
+void dfs_mod(vector<vector<int>>& graph, vector<vector<int>>& res, int curr_node, vector<int>& path){
+    int n = graph.size();
+    for(int node : graph[curr_node]){
+        path.push_back(node);
+        if(node == n-1){
+            res.push_back(path);
+        }
+        dfs_mod(graph, res, node, path);
+        path.erase(--path.end());
+    }
+}
+vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    vector<vector<int>> result;
+    vector<int> path;
+    path.push_back(0);
+    dfs_mod(graph, result, 0,path);
+    return result;
+}
+
+};
