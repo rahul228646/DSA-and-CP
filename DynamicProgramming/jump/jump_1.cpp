37aadb3abff5cc2f2d24d24825560e0ea49c6b52
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-01 13:05:27 +0530

    Create jump_1.cpp

---
+++ DynamicProgramming/jump/jump_1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int step = nums[0];
+        for(int i = 1; i<n; i++) {
+            step--;
+            if(step<0) return false;
+            step = max(nums[i], step);
+        }
+        return true;
+    }
+};
