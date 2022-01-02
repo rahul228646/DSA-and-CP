326b2b1407000e873b4c9af39c8c6e7f3a625daa
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-03 02:18:09 +0530

    Update LCA.cpp

--- Trees/BST/LCA.cpp
+++ Trees/BST/LCA.cpp
@@ -14,3 +14,17 @@ public:
     
     }
 };
+
+// for Binary Tree
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return root;
+        if(root == p || root == q) return root; // if root is q or q return root
+        TreeNode* l = lowestCommonAncestor(root->left, p, q); // look for q or p in left 
+        TreeNode* r = lowestCommonAncestor(root->right, p, q); // look for q or p in right 
+        if(l && r) return root; // if p and q exist in different subtrees then l and r would be non null and root is the lca
+        return l? l:r; // p and q are in the same subtree 
+    }
+};
