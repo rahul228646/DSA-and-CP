a12b8d711680e48823af8615869999d4cb86daca
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-03 01:54:25 +0530

    Update LCA.cpp

--- Trees/BST/LCA.cpp
+++ Trees/BST/LCA.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* val1, TreeNode* val2) {
-            if(root == NULL)
-        return NULL;
+        if(root == NULL)
+            return NULL;
 
+        if((root->val > val1->val && root->val > val2->val)) // val1 and val2 are in left subtree
+            return lowestCommonAncestor(root->left ,val1,val2);
     
-    if(root == val1 || root == val2)
-        return root;
+        if((root->val < val1->val && root->val < val2->val)) // val1 and val2 are in right subtree
+            return lowestCommonAncestor(root->right ,val1,val2);
+        
+        return root; // val1 and val2 are in different subtrees i.e. root is the lowest common ancestor
     
-    else if((root->val > val1->val && root->val > val2->val))
-         return lowestCommonAncestor(root->left ,val1,val2);
-    
-    else if((root->val < val1->val && root->val < val2->val))
-        return lowestCommonAncestor(root->right ,val1,val2);
-    
-    else {
-        TreeNode * a = lowestCommonAncestor(root->left ,val1,val2);
-        TreeNode * b = lowestCommonAncestor(root->right ,val1,val2);
-    
-    if(a!=NULL && b!=NULL)
-        return root;
-    else if(a==NULL && b==NULL)
-        return NULL;
-    else if(a!=NULL && b==NULL)
-        return a;
-    else
-        return b;
-    }
     }
 };
