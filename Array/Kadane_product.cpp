dd5ab8da0af17d62ee330f9685712e4b1b9344b6
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-03 12:16:12 +0530

    Update Kadane_product.cpp

--- Array/Kadane_product.cpp
+++ Array/Kadane_product.cpp
@@ -1,4 +1,4 @@
-
+// https://leetcode.com/problems/maximum-product-subarray/discuss/1608800/C%2B%2B-or-Discussion-in-detail-or-Easy-to-understand
 class Solution {
 public:
    int maxProduct(vector<int>& nums) {
