5be2d751f7c7650eed2c42f1bf45051cd540e8ec
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-07 04:47:00 +0530

    Update decode_ways.cpp

--- DynamicProgramming/decode_ways.cpp
+++ DynamicProgramming/decode_ways.cpp
@@ -23,3 +23,25 @@ public:
 };
 
 // or
+
+class Solution {
+public:
+    
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp (n+1, 0);
+        dp[0] = 1;
+        if(s[0]=='0') return 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++) {
+            int way1,way2;
+            if(s[i-1]=='0')way1=0;
+            else way1=dp[i-1];
+            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
+            else way2=0;
+            dp[i]=way1+way2;
+            
+        }
+        return dp[n];
+    }
+};
