586905926a9513f08fad5ce3effdebb4f9d9157f
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-09 18:25:56 +0530

    Create  Create minSwap_to_bring_target_element_together_2.cpp

---
+++ Sliding_Window/Create minSwap_to_bring_target_element_together_2.cpp
@@ -0,0 +1,37 @@
+
+here array is circular array of 1's and 0's
+https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/submissions/  
+
+class Solution {
+public:
+    int minSwapTogether(vector<int>& arr, int t) {
+        
+        int n = arr.size(), targetCount = 0, maxTargetCount = 0;
+        
+        for(auto i : arr) if(i == t) targetCount++;
+        
+        int windowSize = targetCount, windowTargetCount = 0;
+        
+        for(int i = 0; i<windowSize; i++) if(arr[i] == t) windowTargetCount++;
+        
+        int i = 0, j = windowSize;
+        
+        maxTargetCount =  windowTargetCount;
+        while(j<n-1) {
+            if(arr[i] == t) windowTargetCount--;
+            if(arr[j] == t) windowTargetCount++;
+            maxTargetCount = max(windowTargetCount, maxTargetCount);
+            i++;
+            j++;
+        }
+        
+        return targetCount - maxTargetCount;
+    } 
+    int minSwaps(vector<int>& arr) {
+        int oneSwap =  minSwapTogether(arr, 1);
+        // cout<<oneSwap<<endl;
+        int zeroSwap =  minSwapTogether(arr, 0);
+        // cout<<zeroSwap<<endl;
+        return min(oneSwap, zeroSwap);
+    }
+};
