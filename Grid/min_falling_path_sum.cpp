774477b1534b4df4aad5ee52cebcc557d31c003f
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-10 10:32:15 +0530

    Update min_falling_path_sum.cpp

--- Grid/min_falling_path_sum.cpp
+++ Grid/min_falling_path_sum.cpp
@@ -24,3 +24,49 @@ public:
         return ans;
     }
 };
+
+// memo
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    vector<pair<int, int>> dir = {{1, 0}, {1, 1}, {1, -1}};
+    int solve (vector<vector<int>>& mat, int i, int j, int n, int m) {
+        if(i<0 || j<0 || i>=n || j>=m) return INT_MAX;
+        if(i == n-1) return mat[i][j];
+        if(dp[i][j] != INT_MAX) return dp[i][j];
+        int ans = INT_MAX;
+        for(auto k : dir) ans = min(ans,solve(mat, i+k.first,  j+k.second, n, m));
+        return dp[i][j] = mat[i][j]+ans;
+    }
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        int ans = INT_MAX;
+        dp = vector<vector<int>>(n+1, vector<int>(m+1, INT_MAX) );
+        for(int i = 0; i<n; i++) ans = min(ans, solve(mat, 0,  i, n, m)); 
+        return ans;
+    }
+};
+
+
+// tabular or bottom up 
+// Time : O(N^2) Space : O(1)
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        int ans = INT_MAX;
+        for(int i = 1; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                int a = mat[i-1][j], b = INT_MAX, c = INT_MAX;
+                if(j-1 >= 0) b = mat[i-1][j-1];
+                if(j+1 < m) c = mat[i-1][j+1]; 
+                mat[i][j] += min(a, min(b, c));
+            }
+        }
+        for(auto i : mat[n-1]) ans = min(ans, i);
+        return ans;
+    }
+};
+
