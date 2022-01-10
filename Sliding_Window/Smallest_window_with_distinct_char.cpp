472655401c53c1f7b007e75f5f42f3a471670647
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-10 13:53:27 +0530

    Create Smallest_window_with_distinct_char.cpp

---
+++ Sliding_Window/Smallest_window_with_distinct_char.cpp
@@ -0,0 +1,29 @@
+string findSubString(string str)
+    {
+        unordered_map<char, int> m;
+        for (auto i : str) m[i]++;
+        int count = 0, n = str.length(), j = 0, i = 0, mx = INT_MAX;
+        string ans;
+        count = m.size();
+        
+        m.clear();
+        while(j<n) {
+            if(m.size() < count) {
+                m[str[j]]++;
+                
+            }
+            while(m.size() == count) {
+                if(mx > j-i+1) {
+                    mx = j-i+1;
+                    ans = str.substr(i, j-i+1);
+                }
+                m[str[i]]--;
+                if(m[str[i]] == 0) m.erase(str[i]);
+                
+                i++;   
+            }
+          
+            j++;
+        }
+        return ans;
+    }
