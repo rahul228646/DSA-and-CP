6fedba019b70d7bb064d8d8576b1de5561865534
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-11 11:45:40 +0530

    Update print_all_anagrams_together.cpp

--- String/Anagrams/print_all_anagrams_together.cpp
+++ String/Anagrams/print_all_anagrams_together.cpp
@@ -14,3 +14,22 @@ public:
         return ans;
     }
 };
+
+// time : O(N * M) space : O(M * N)
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        vector<vector<string>> ans;
+        map<map<char, int>, vector<string>> mp;
+        for(auto s : str) {
+            map<char, int> temp;
+            for(auto c : s) {
+                temp[c]++;
+            }
+            mp[temp].push_back(s);
+        }
+        for(auto i : mp) ans.push_back(i.second);
+        return ans;
+    }
+};
