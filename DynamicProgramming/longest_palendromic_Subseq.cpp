8f7bb59706f54f9c0c6f2f4d87591474940e9b17
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-14 10:25:48 +0530

    Update longest_palendromic_Subseq.cpp

--- DynamicProgramming/longest_palendromic_Subseq.cpp
+++ DynamicProgramming/longest_palendromic_Subseq.cpp
@@ -26,3 +26,32 @@ public:
        return longestPalindromicSubsequenceHelper(s, a);
    }
 };
+
+// gap startegy
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length(), mx = 0;
+        vector<vector<int>> dp (n, vector<int>(n, 0));
+        for(int gap = 0; gap<n; gap++) {
+            for(int i = 0, j = gap; j<n; i++, j++) {
+                if(gap == 0)            
+                    dp[i][j] =1;   
+                else if(gap ==1 && s[i] == s[j])       
+                    dp[i][j] = 2;
+                else {
+                    if(s[i]==s[j] && dp[i+1][j-1] != 0) dp[i][j] = 2+dp[i+1][j-1]; 
+                    else  dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+                }
+                mx = max(mx, dp[i][j]);
+            }
+        } 
+        // for(auto i : dp) {
+        //     for(auto j : i) cout<<j<<" ";
+        //     cout<<endl;
+        // }
+
+        return mx;
+    }
+};
