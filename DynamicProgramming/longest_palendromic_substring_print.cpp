d113e6f65c007e19d24aada15e5d6d4b9a461d1a
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-14 10:24:45 +0530

    Update longest_palendromic_substring_print.cpp

--- DynamicProgramming/longest_palendromic_substring_print.cpp
+++ DynamicProgramming/longest_palendromic_substring_print.cpp
@@ -7,8 +7,8 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, 0));
         for(int gap = 0; gap<n; gap++) {
             for(int i = 0, j = gap; j<n; i++, j++) {
-                if(i == j) dp[i][j] = 1;
-                else if(j == i+1) dp[i][j] = (s[i]==s[j]) ? 2 : 0;
+                if(gap == 0) dp[i][j] = 1;
+                else if(gap == 1) dp[i][j] = (s[i]==s[j]) ? 2 : 0;
                 else {
                     if(s[i]==s[j] && dp[i+1][j-1] != 0) dp[i][j] = 2+dp[i+1][j-1];
                     else dp[i][j] = 0;
