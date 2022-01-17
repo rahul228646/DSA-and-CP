61781ff7cb45d5f2da7749859b2fa5c47fbfccb8
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-17 19:04:05 +0530

    Update minimum_limit_of_balls_in_a_bag.cpp

--- BinarySearch/minimum_limit_of_balls_in_a_bag.cpp
+++ BinarySearch/minimum_limit_of_balls_in_a_bag.cpp
@@ -29,3 +29,26 @@ public:
     }
     
 };
+
+// mid depicts the value of penalty is valid determines if this is the max penalty possible, for MaxOperations
+// For example, the mid = 3,
+// A[i] = 2, we split it into [2], and operations = 0
+// A[i] = 3, we split it into [3], and operations = 0
+// A[i] = 4, we split it into [3,1], and operations = 1
+// A[i] = 5, we split it into [3,2], and operations = 1
+// A[i] = 6, we split it into [3,3], and operations = 1
+// A[i] = 7, we split it into [3,3,1], and operations = 2
+
+// The number of operation we need is (a - 1) / mid
+
+// If the total operation > max operations,
+// the size of bag is too small,
+// we set left = mid + 1
+
+// Otherwise,
+// this size of bag is big enough,
+// we set right = mid
+
+// We return the final result,
+// where result = left = right.
+
