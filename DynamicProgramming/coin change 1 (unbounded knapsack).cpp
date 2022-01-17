9e27979976dbea29a4aeb0d78cd3ed3b5998da33
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-17 12:46:42 +0530

    Update coin change 1 (unbounded knapsack).cpp

--- DynamicProgramming/coin change 1 (unbounded knapsack).cpp
+++ DynamicProgramming/coin change 1 (unbounded knapsack).cpp
@@ -29,3 +29,31 @@ public:
         return (ans == INT_MAX || ans == INT_MAX-1) ? -1 : ans;
     }
 };
+
+// tabular
+
+class Solution {
+public:
+
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp (n+1, vector<int>(amount+1, 0));
+        
+        dp[0][0] = INT_MAX-1;
+        for(int i = 1; i<=n; i++) dp[i][0] = 0; // ans is 0 if amount == 0;
+        for(int i = 1; i<=amount; i++) dp[0][i] = INT_MAX-1; 
+        // ans is -1 for n == 0 but since we will be doing min(1+including,  exluding) so we will keep it as INT_MAX-1 and put a check for it at the end;
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=amount; j++) {
+                if(coins[i-1]<=j) {
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (dp[n][amount] == INT_MAX || dp[n][amount]  == INT_MAX-1) ? -1 : dp[n][amount];
+    }
+};
