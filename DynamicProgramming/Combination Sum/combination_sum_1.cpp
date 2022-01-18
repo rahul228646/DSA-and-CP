06b9d6ac4f9eb8f9611e3e033bfb6d89ff6440df
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-18 12:15:40 +0530

    Update and rename DynamicProgramming/CombinationSum_1.cpp to DynamicProgramming/Combination Sum/combination_sum_1.cpp

---
+++ DynamicProgramming/Combination Sum/combination_sum_1.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/combination-sum/
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve (vector<int> c, int t, int n, vector<int> &a) {
+       if(t == 0) {
+           ans.push_back(a);
+           return;
+       }
+       if(n<=0)
+           return;
+      
+       if(c[n-1] <= t) {
+           a.push_back(c[n-1]);
+           solve(c, t-c[n-1], n, a);
+           a.pop_back();
+           solve(c, t, n-1, a);
+       }
+       else {
+            solve(c, t, n-1, a);
+       }
+   }
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        int n = c.size();
+        vector<int> a;
+        solve(c, t, n, a);
+        return ans;
+    }
+};
