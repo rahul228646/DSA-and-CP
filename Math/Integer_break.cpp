5605ecb3836aeef6699cd0f301d459ca8c5d52c6
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-18 16:27:53 +0530

    Create Integer_break.cpp

---
+++ Math/Integer_break.cpp
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/integer-break/
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        int divide= n/3;
+        int remainder=n%3;
+        int product=1;
+        if(remainder==0) return pow(3,divide);
+        if(remainder ==1) return pow(3,divide-1)*4;
+        if(remainder==2) return pow(3,divide)*2;
+        return 0;
+        
+    }
+};
+
+I saw many solutions were referring to factors of 2 and 3. But why these two magic numbers? Why other factors do not work?
+Let's study the math behind it.
+
+For convenience, say n is sufficiently large and can be broken into any smaller real positive numbers. We now try to calculate which real number generates the largest product.
+Assume we break n into (n / x) x's, then the product will be xn/x, and we want to maximize it.
+
+Taking its derivative gives us n * xn/x-2 * (1 - ln(x)).
+The derivative is positive when 0 < x < e, and equal to 0 when x = e, then becomes negative when x > e,
+which indicates that the product increases as x increases, then reaches its maximum when x = e, then starts dropping.
+
+This reveals the fact that if n is sufficiently large and we are allowed to break n into real numbers,
+the best idea is to break it into nearly all e's.
+On the other hand, if n is sufficiently large and we can only break n into integers, we should choose integers that are closer to e.
+The only potential candidates are 2 and 3 since 2 < e < 3, but we will generally prefer 3 to 2. Why?
+
+Of course, one can prove it based on the formula above, but there is a more natural way shown as follows.
+
+6 = 2 + 2 + 2 = 3 + 3. But 2 * 2 * 2 < 3 * 3.
+Therefore, if there are three 2's in the decomposition, we can replace them by two 3's to gain a larger product.
+
+All the analysis above assumes n is significantly large. When n is small (say n <= 10), it may contain flaws.
+For instance, when n = 4, we have 2 * 2 > 3 * 1.
+To fix it, we keep breaking n into 3's until n gets smaller than 10, then solve the problem by brute-force.
