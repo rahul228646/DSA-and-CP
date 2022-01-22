df11e27d70a912971e740852d79bb047551f5621
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-01-22 16:25:02 +0530

    Update find_target_in_2D_matrix.cpp

--- BinarySearch/find_target_in_2D_matrix.cpp
+++ BinarySearch/find_target_in_2D_matrix.cpp
@@ -1,5 +1,8 @@
-
+// https://leetcode.com/problems/search-a-2d-matrix-i/
 // Time complexity : log(nm)
+// each row is sorted and next element of each row is gretaer than the last element of previous row
+// 1 2 3 4
+// 5 6 7 8
 
 class Solution {
 public:
@@ -34,3 +37,26 @@ public:
         return false;
     }
 };
+
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+// time is O(m+n)
+// each row and column are sorted independentaly
+class Solution {
+public:
+
+    bool searchMatrix(vector<vector<int>>& m, int t) {
+        int r = 0, c = m[0].size()-1;
+        while(r<m.size() && c>=0) {
+            if(m[r][c] == t) return true;
+            if(m[r][c] > t) c--;
+            else {
+                r++;
+            }
+        }
+        return false;
+    }
+};
+
+// we from the first element of the last column target is smaller than this. it means that target is not in this column, we do c-- else if target is greater than this value. it means that target could be in this column so we do r++;
+// repeat this until we find the target;
+
