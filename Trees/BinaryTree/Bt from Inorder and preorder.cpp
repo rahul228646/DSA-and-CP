e9a50c92077c1c198ff370ed100ed30c24211d41
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-02-02 12:25:26 +0530

    Create Bt from Inorder and preorder.cpp

---
+++ Trees/BinaryTree/Bt from Inorder and preorder.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+// time : O(N^2) (because we have to search rootIdx)
+class Solution {
+public:
+    TreeNode* solve(vector<int>& pre, vector<int>& in, int inS, int inE, int preS, int preE) {
+        if(inS>inE) return nullptr;
+        int rootData = pre[preS];
+        int rootIdx = -1;
+        for(int i = inS; i<=inE; i++) {
+            if(rootData == in[i]) {
+                rootIdx = i;
+                break;
+            }
+            
+        }
+        int lInS = inS;
+        int lInE = rootIdx-1;
+        int lPreS = preS+1;
+        int lPreE = preS+(lInE-lInS)+1;
+        int rInS = rootIdx+1;
+        int rInE = inE;
+        int rPreS = lPreE+1;
+        int rPreE = preE;
+        TreeNode* root = new TreeNode(rootData);
+        TreeNode* left = solve(pre, in,  lInS, lInE, lPreS, lPreE);
+        TreeNode* right = solve(pre, in, rInS, rInE, rPreS, rPreE);    
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        return solve(pre, in, 0, in.size()-1, 0, pre.size()-1);
+    }
+};
+
+// prerder--> root left right
+// inorder--> left root right
