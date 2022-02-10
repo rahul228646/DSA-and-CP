3533d156a2b718263b61d9c77af5ffb2d527f725
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-02-10 17:12:39 +0530

    Update path_source_to_end(DAG).cpp

--- Graphs/path_source_to_end(DAG).cpp
+++ Graphs/path_source_to_end(DAG).cpp
@@ -1,31 +1,7 @@
 //DAG = Directed Acyclic Graph
 //https://leetcode.com/problems/all-paths-from-source-to-target/
 
-class Solution {
-public:
-void dfs_mod(vector<vector<int>>& graph, vector<vector<int>>& res, int curr_node, vector<int>& path){
-    int n = graph.size();
-    for(int node : graph[curr_node]){
-        path.push_back(node);
-        if(node == n-1){
-            res.push_back(path);
-        }
-        dfs_mod(graph, res, node, path);
-        path.erase(--path.end());
-    }
-}
-vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-    vector<vector<int>> result;
-    vector<int> path;
-    path.push_back(0);
-    dfs_mod(graph, result, 0,path);
-    return result;
-}
-
-};
-
-// or
-
+// DFS
 class Solution {
 public:
     vector<vector<int>> result;
@@ -43,5 +19,30 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {\
         dfs(graph, 0, graph.size()-1);
         return result;
+    }
+};
+
+// BFS
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int start = 0, end = graph.size()-1;
+        queue<pair<int, vector<int>>> q;
+        vector<vector<int>> ans;
+        q.push({start, {start}});
+
+        while(!q.empty()) {
+            auto [node, path] = q.front();
+            q.pop();
+            if(node == end) {
+                ans.push_back(path);
+            }
+            for(auto i : graph[node]) {
+                path.push_back(i);
+                q.push({i, path});
+                path.pop_back();
+            }
+        }
+        return ans;
     }
 };
