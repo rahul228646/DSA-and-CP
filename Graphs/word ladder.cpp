8165e50ef9fa9ef8b8e9384812482368b21a37f0
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-02-12 13:38:45 +0530

    Create word ladder.cpp

---
+++ Graphs/word ladder.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int ladderLength(string start, string end, vector<string>& wl) {
+        unordered_set<string> dict(wl.begin(), wl.end());
+        if(dict.find(end) == dict.end()) return 0;
+        queue<string> q;
+        q.push(start);
+        int ans = 1;
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0; i<n; i++) {
+                auto curr = q.front();
+                q.pop();
+                if(curr == end) return ans;
+                for(int j = 0; j<curr.size(); j++) {
+                    char c = curr[j];
+                    for(int k = 0; k<26; k++) {
+                        curr[j] = 'a'+k;
+                        if (dict.find(curr) != dict.end()) {
+                            q.push(curr);
+                            dict.erase(curr);
+                        }
+                    }
+                    curr[j] = c;
+                }
+                
+            }
+            ans++;
+        }
+        return 0;
+    }
+};
