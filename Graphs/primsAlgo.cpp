f8147792bddec22a6deb4e6fcd36a6889105ab46
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-02-15 00:01:34 +0530

    Update primsAlgo.cpp

--- Graphs/primsAlgo.cpp
+++ Graphs/primsAlgo.cpp
@@ -1,37 +1,45 @@
 
-Time: O(V^2)
+Time: O(E.logV)
 Space: O(V)
-// adj[u] is vector with v source and w weight
-class Prim1{
-  public:
-  	int findNodeWithMinKey(vector<bool> setMst, vector<int> key, int V) {
-        int min = INT_MAX, min_index;
-     
-        for (int v = 0; v < V; v++)
-            if (setMst[v] == false && key[v] < min)
-                min = key[v], min_index = v;
-     
-        return min_index;
-	  }
-    int spanningTree(int V, vector<vector<int>> adj[]) {
-        // code here
-        vector<int> parent(V, -1), key(V, INT_MAX);
-        vector<bool> setMst(V, false);
-        key[0] = 0;
-        // since mst has v-1 edges
-        for(int i = 0; i<V-1; i++) {
-            int u = findNodeWithMinKey(setMst, key, V);
-            setMst[u] = true;
-            for(auto v : adj[u]) {
-                if((v[0]) && (setMst[v[0]] == false) && (v[1]<key[v[0]])) {
-                    parent[v[0]] = u, key[v[0]] = v[1];
+
+// “Kruskal’s algorithm” expands the “minimum spanning tree” by adding edges. 
+//  Whereas “Prim’s algorithm” expands the “minimum spanning tree” by adding vertices.
+	
+class Solution {
+public:
+  
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n = points.size();
+        vector<bool> visited(n, false);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        
+        for(int i = 1; i<n; i++) {
+            int val = abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]); // calc all cost from vertex 1 and add to queue {cost, vertex1, vertexi}
+            pq.push({val, {0, i}});
+        }
+
+        visited[0] = true; // mark 1st vertex as visited
+        int k = 0, cost = 0;
+        
+        while(!pq.empty() && k<n-1) {
+            auto [val, vertices]= pq.top();
+            auto [x, y] = vertices;
+            pq.pop();
+            if(!visited[y]) { // selecte the ith (which is on top of min heap) and calc. and add other cots of other unvisited vertex from it
+                visited[y] = true;
+                cost += val;
+                k++;
+                for(int i = 0; i<n; i++) {
+                    if(!visited[i]) {
+                        int temp_val = abs(points[i][0] - points[y][0]) + abs(points[i][1] - points[y][1]);
+                        pq.push({temp_val, {y, i}});
+                    }
                 }
             }
         }
-        int sum = 0;
-        for(int i = 0; i<V; i++) {
-            sum += key[i];
-        }
-        return sum;
+        
+        return cost;
+        
     }
 };
