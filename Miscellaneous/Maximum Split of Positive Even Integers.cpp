15838c8a437fb927e8c68be7cc59c476c0e15c8b
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-02-19 22:25:52 +0530

    Create Maximum Split of Positive Even Integers.cpp

---
+++ Miscellaneous/Maximum Split of Positive Even Integers.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-split-of-positive-even-integers/submissions/
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int idx, int n, int k, vector<int>& curr) {
+        if(curr.size()==k) {
+            ans.push_back(curr);
+            return ;
+        }
+        for(int i = idx; i<=n; i++) {
+            curr.push_back(i);
+            solve(i+1, n, k, curr);
+            curr.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> curr;
+        solve(1, n, k, curr);
+        return ans;
+    }
+};
+
