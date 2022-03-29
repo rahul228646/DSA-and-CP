a54d9e98c7973a8b5043ebebf1d250eb989cc503
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-03-30 00:53:10 +0530

    Update find duplicate_binary_search.cpp

--- Binary Search/find duplicate_binary_search.cpp
+++ Binary Search/find duplicate_binary_search.cpp
@@ -1,4 +1,14 @@
+// https://leetcode.com/problems/find-the-duplicate-number/solution/
 
+// Consider an array that has n distinct numbers in the range [1,n]. For example: 
+// [1,2,3,4,5]. If we pick any one of these 5 numbers and count how many numbers are less than or equal to it, 
+// the answer will be equal to that number. So in [1,2,3,4,5], if you pick the number 4, there's exactly 4 numbers that are less than or equal to 
+// If you pick 3 there's exactly 3 numbers that are less than or equal to 3, and so on. 
+// However, when you have duplicates in the array, this count will exceed the number at some point. For example: in 
+// [4,3,4,5,2,4,1] 3 has 3 numbers less than or equal to it. However, the duplicate number will have a count of numbers less than or equal to itself, 
+// that is greater than itself (in this example, 4 which is the duplicate, has 6 numbers that are less than or equal to it). Hence, 
+// the smallest number that satisfies this property is the duplicate number.
+  
 class Solution {
 public:
     int smallorequal(int mid, vector<int> arr) {
