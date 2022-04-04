7fbfd170a65b3810b5dc7527426df8f872b14437
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-04-04 22:33:25 +0530

    Update number of ways to select offices and restaurants.cpp

--- Dynamic Programming/number of ways to select offices and restaurants.cpp
+++ Dynamic Programming/number of ways to select offices and restaurants.cpp
@@ -21,3 +21,29 @@ public:
         return solve(s, '2', s.length(), 0);
     }
 };
+
+
+// https://leetcode.com/problems/number-of-ways-to-select-buildings/discuss/1907026/Easy-understanding-C%2B%2B-code-with-comments
+
+class Solution {
+public:
+    long long numberOfWays(string s) {
+        int oneCount = 0, zeroCount = 0;
+        for(auto i : s) if(i == '1') oneCount++; else zeroCount++;
+        int oneBeforeZero = 0, zeroBeforeOne = 0;
+        long long ans = 0;
+        for(auto i : s) {
+            if(i == '1') {
+                ans += zeroBeforeOne * zeroCount; // zeroCount == zeroAfterOne
+                oneCount--;
+                oneBeforeZero++;
+            }
+            else {
+                ans += oneBeforeZero * oneCount; // oneCount == oneAfterZero
+                zeroCount--;
+                zeroBeforeOne++;
+            }
+        }
+        return ans;
+    }
+};
