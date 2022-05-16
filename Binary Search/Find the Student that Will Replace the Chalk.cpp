732083a84b717d190ce24cba76d3588e7e369eb7
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-17 04:24:37 +0530

    Create Find the Student that Will Replace the Chalk.cpp

---
+++ Binary Search/Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/
+
+class Solution {
+public:
+    int binarySearch(vector<long long> prefix, int k, int n) {
+        int lo = 0, hi = n-1, mid = 0;
+        while(lo<=hi) {
+            mid = (lo+hi)/2;
+            if(prefix[mid] <= k) lo = mid+1;
+            else hi = mid-1;
+        }
+        return lo;
+    }
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n = chalk.size();
+        long long sum = 0;
+        vector<long long> prefix;
+        for(auto i : chalk) {
+            sum += i;
+            prefix.push_back(sum);
+        }
+        k = k % sum;
+        return binarySearch(prefix, k, n);
+    }
+};
+
+chalk = [3,4,1,2], k = 25
+  
+prefix = 3 7 8 10 ; k % sum = 5 
