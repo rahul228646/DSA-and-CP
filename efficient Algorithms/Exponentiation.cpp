b56717bdf758330a07c62cac95fe8e06aa005758
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-26 05:37:40 +0530

    Create Exponentiation.cpp

---
+++ efficient Algorithms/Exponentiation.cpp
@@ -0,0 +1,23 @@
+int power(int x, int y)
+{
+     
+    // Initialize answer
+    int res = 1;
+     
+    // Check till the number becomes zero
+    while (y)
+    {
+         
+        // If y is odd, multiply x with result
+        if (y % 2 == 1)
+            res = (res * x);
+             
+        // y = y/2
+        y = y >> 1;
+         
+        // Change x to x^2
+        x = (x * x);
+    }
+    return res;
+}
+ 
