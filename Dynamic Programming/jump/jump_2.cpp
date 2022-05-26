bebe94b5fdd805a3cce8224ab3fbcb9935ad2e2e
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-26 13:19:27 +0530

    Update jump_2.cpp

--- Dynamic Programming/jump/jump_2.cpp
+++ Dynamic Programming/jump/jump_2.cpp
@@ -55,3 +55,28 @@ int jump(vector<int>& nums) {
 	return jumps;
 }
 
+
+class Solution {
+public:
+    
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1) return 0;
+        if(nums[0] == 0) return 0;
+        int maxReach = nums[0], steps = nums[0], jumps = 1;
+        for(int i = 1; i<n; i++) {
+            if(i == n-1) return jumps;
+            maxReach = max(maxReach, i+nums[i]);
+            steps--;
+            if(!steps) {
+                jumps++;
+                if(maxReach <= i) return -1;
+                steps = maxReach-i;
+                
+            }
+        }
+        return -1;
+    }
+};
+
+
