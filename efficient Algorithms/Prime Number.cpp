2a0072e208d1f02d4caea84398029ca6b7a3f635
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-26 05:50:51 +0530

    Create Prime Number.cpp

---
+++ efficient Algorithms/Prime Number.cpp
@@ -0,0 +1,31 @@
+// Sieve Of Eratosthenes - given all the prime no less than a given number
+
+Time Complexity: O(n*log(log(n)))
+
+Auxiliary Space: O(n)
+  
+void SieveOfEratosthenes(int n)
+{
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p = 2; p * p <= n; p++) {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true) {
+            // Update all multiples of p greater than or
+            // equal to the square of it numbers which are
+            // multiple of p and are less than p^2 are
+            // already been marked.
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+ 
+    // Print all prime numbers
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
