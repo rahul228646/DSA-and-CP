60ba7035827f7b50f25ec9ab34b54d02f4ec6d95
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-30 17:03:51 +0530

    Update and rename Stack/DailyTemp.cpp to Stack/Monotonic Stack/DailyTemp.cpp

--- Stack/Monotonic Stack/DailyTemp.cpp
+++ Stack/Monotonic Stack/DailyTemp.cpp
@@ -5,20 +5,15 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
+        stack<int> st; // decreasing stack
         int n = t.size();
-        vector <int> ans(n, 0); 
-        stack <int> st;
-        for(int i = n-1; i>=0; i--) {
-           
-            if(st.empty()) {
-                ans[i] = 0;
-                st.push(i);
-            }
-            
-            while(!st.empty() && t[i] >= t[st.top()]) {
+        vector<int> ans (n, 0);
+        st.push(0);
+        for(int i = 1; i<n; i++) {
+            while(st.size() && t[st.top()]<t[i]) {
+                ans[st.top()] = i-st.top();
                 st.pop();
             }
-            ans[i] = st.empty() ? 0 : st.top()-i;
             st.push(i);
         }
         return ans;
