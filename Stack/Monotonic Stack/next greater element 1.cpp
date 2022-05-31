bc2e4778b991123681d06fb5109492202fc71906
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-05-31 07:27:42 +0530

    Create next greater element 1.cpp

---
+++ Stack/Monotonic Stack/next greater element 1.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/next-greater-element-i/
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+     
+        unordered_map<int, int> m;
+        vector<int> ans;
+        stack<int> st;
+        for(auto i : nums2) {
+            while(!st.empty() && st.top() < i) {
+                m[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        for(auto i : nums1) (m.find(i) != m.end()) ? ans.push_back(m[i]) : ans.push_back(-1);
+        return ans;
+    }
+};
