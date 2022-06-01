a1d64a0035f14be3124043b8676e497a20436124
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-06-01 20:20:33 +0530

    Update coin change 1.cpp

--- Dynamic Programming/coin change (unbounded knapsack)/coin change 1.cpp
+++ Dynamic Programming/coin change (unbounded knapsack)/coin change 1.cpp
@@ -4,28 +4,19 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(vector<int> &coins, int n, int amount) {
-      
-        if(n <= 0 && amount != 0) return INT_MAX-1;
-        if(amount == 0) return 0;
+    int solve(vector<int>& coins, int amount, int n) {
+        if(t == 0) return 0;
+        if(n == 0) return INT_MAX-1;
         if(dp[n][amount] != -1) return dp[n][amount];
-      
-        if(coins[n-1] <= amount) {
-            int a = 1+solve(coins, n, amount-coins[n-1]); // each coin can be included any number of time to we are calling with n insteas of n-1
-            int b = solve(coins, n-1, amount);
-          
-            if(a == b && (a == INT_MAX-1 || a == INT_MAX)) return dp[n][amount] = INT_MAX-1;
-            else 
-                return dp[n][amount] = min(a, b);
-        }
-        else {
-            return dp[n][amount] = solve(coins, n-1, amount);
-        }
+        if(coins[n-1] <= amount)
+            return dp[n][amount] = min(1 + solve(coins, amount-coins[n-1], n), solve(coins, amount, n-1));
+        else 
+            return dp[n][amount] = solve(coins, amount, n-1);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         dp = vector<vector<int>> (n+1, vector<int>(amount+1, -1));
-        int ans = solve(coins, n, amount);
+        int ans = solve(coins, amount, n);
         return (ans == INT_MAX || ans == INT_MAX-1) ? -1 : ans;
     }
 };
