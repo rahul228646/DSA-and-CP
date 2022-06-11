106cbec5939bf235e62faf263825dff88d2117d6
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-06-11 23:22:01 +0530

    Create Count Subarrays With Score Less Than K.cpp

---
+++ Array/Subarray Related/Count Subarrays With Score Less Than K.cpp
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/count-subarrays-with-score-less-than-k/
+
+long long countSubarrays(vector<int>& nums, long long k) {
+    long long sum = 0, res = 0;
+    for (int i = 0, j = 0; i < nums.size(); ++i) {
+        sum += nums[i];
+        while (sum * (i - j + 1) >= k)
+            sum -= nums[j++];
+        res += i - j + 1;
+    }
+    return res;
+}
