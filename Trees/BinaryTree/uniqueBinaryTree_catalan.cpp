43407c7832270aa0a55df03396f3f4ed219d69c9
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-06-14 11:58:21 +0530

    Update uniqueBinaryTree_catalan.cpp

--- Trees/BinaryTree/uniqueBinaryTree_catalan.cpp
+++ Trees/BinaryTree/uniqueBinaryTree_catalan.cpp
@@ -2,6 +2,28 @@
 
 // catalan number : https://leetcode.com/problems/unique-binary-search-trees/
 
+// Case 1 : n = 1 Only one combination is possible.
+
+// Case 2 : n = 2 Let fix one node. Two possibilities for second node. It can either be on right or left.
+// Hence for n=2 , 2 trees possible
+
+// Case 3: n = 3
+// Again fix one node, and we are left with two nodes.
+// Possibilities :
+
+// Both are on right
+// Both are on left
+// One on right and other on left
+// Answer to each above case will be Combinations on right X Combinations on left.
+// Now we know answer of TWO nodes = 2
+// Answer for ONE node is 1
+// Answer for THREE nodes : ( ( 2 on right X 0 on left ) + ( 1 on right X 1 on left ) +( 0 on right X 2 on left ) )
+
+// Other CASES :
+// SIMILARLY a problem with n nodes can be broken into for n - 1 nodes.
+
+// Array a is created for memomization so that repeated recurcive calls for same value of n are prevented.
+
 // T(i) = summation 0 : i-1 T(j) * T(i-j-1)
 // Time : O(n) Space : O(n)
 
