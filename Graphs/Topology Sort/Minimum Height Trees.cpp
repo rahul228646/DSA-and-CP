0368cc5032f9cb37ef8ff15b504b9e66552d18f7
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-02 15:17:02 +0530

    Create Minimum Height Trees.cpp

---
+++ Graphs/Topology Sort/Minimum Height Trees.cpp
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/minimum-height-trees
+
+class Solution {
+public:
+    
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n==1)    
+            return {0};
+        vector<int> adj[n];
+        vector<int> ans;
+        vector<int> in(n, 0);
+        queue<int> q;
+        for(auto i : edges) {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+            in[i[0]]++;
+            in[i[1]]++;
+        }
+        for(int i = 0; i<n; i++) {
+            if(in[i] == 1) q.push(i);
+        }
+        while(q.size()) {
+            int sz = q.size();
+            ans.clear();
+            while(sz--) {
+                auto curr = q.front();
+                q.pop();
+                ans.push_back(curr);
+                for(auto i : adj[curr]) {
+                    in[i]--;
+                    if(in[i] == 1) q.push(i);
+                }
+            }
+        }
+        return ans;
+    }
+};
+  
