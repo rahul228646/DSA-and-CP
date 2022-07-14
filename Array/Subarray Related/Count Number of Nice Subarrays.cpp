768c9c08d185d300415e149df9e668a4dfe3e0e4
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-14 14:16:41 +0530

    Create Count Number of Nice Subarrays.cpp

---
+++ Array/Subarray Related/Count Number of Nice Subarrays.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/508217/C%2B%2B%3A-Visual-explanation.-O(1)-space.-Two-pointers
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int j = 0, odd = 0, count = 0, total = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] & 1) {
+                odd++;
+                if (odd >= k) {
+                    count = 1;
+                    while (!(nums[j++] & 1)) count++;
+                    total += count;
+                }
+            } else if (odd >= k) total += count;
+        }
+        return total;
+    }
+};
