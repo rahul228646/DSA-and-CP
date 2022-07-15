6415922707ec65b5a4cc3c1b0743195ad757a495
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-16 02:54:53 +0530

    Update UniquePath-I.cpp

--- Grid/UniquePath-I.cpp
+++ Grid/UniquePath-I.cpp
@@ -42,3 +42,22 @@ public:
         return dp[0][0];
     }
 };
+
+or
+    
+class Solution {
+public:
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0; i<=n; i++) mat[i][0] = 1;
+        for(int i = 0; i<=m; i++) mat[0][i] = 1;
+        
+        for(int i = 1; i<n; i++) {
+            for(int j = 1; j<m; j++) {
+                mat[i][j] = mat[i-1][j] + mat[i][j-1];
+            }
+        }
+        return mat[n-1][m-1];
+    }
+};
