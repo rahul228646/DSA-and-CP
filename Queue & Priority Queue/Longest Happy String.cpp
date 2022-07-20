d38bc10316e75854c16517a75c859f4d90b65c33
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-20 23:14:29 +0530

    Create  Longest Happy String.cpp

---
+++ Queue & Priority Queue/Longest Happy String.cpp
@@ -0,0 +1,46 @@
+// A string s is called happy if it satisfies the following conditions:
+
+// s only contains the letters 'a', 'b', and 'c'.
+// s does not contain any of "aaa", "bbb", or "ccc" as a substring.
+// s contains at most a occurrences of the letter 'a'.
+// s contains at most b occurrences of the letter 'b'.
+// s contains at most c occurrences of the letter 'c'.
+
+// https://leetcode.com/problems/longest-happy-string/
+
+class Solution 
+{    
+public:
+    string longestDiverseString(int a, int b, int c) 
+    {
+        priority_queue<pair<int, char>> pq;
+        if(a != 0) pq.push({ a, 'a' });
+        if(b != 0) pq.push({ b, 'b' });
+        if(c != 0) pq.push({ c, 'c' });
+        char prev1 = '#';
+        char prev2 = '#';
+        string res;
+        while(!pq.empty())
+        {
+            auto [cnt1, ch1] = pq.top(); pq.pop();
+            if(ch1 == prev1 && ch1 == prev2)
+            {
+                if(pq.empty()) return res;
+                auto [cnt2, ch2] = pq.top(); pq.pop();
+                res += ch2;
+                prev1 = prev2;
+                prev2 = ch2;
+                pq.push({ cnt1, ch1 });
+                if(--cnt2 > 0) pq.push({ cnt2, ch2 });
+            }
+            else
+            {
+                prev1 = prev2;
+                prev2 = ch1;
+                res += ch1;
+                if(--cnt1 > 0) pq.push({ cnt1, ch1 });
+            }
+        }
+        return res;
+    }
+};
