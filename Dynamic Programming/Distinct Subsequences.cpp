a599eebf1e392e5bccb37ed4999fcd586bea3cb1
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-23 14:50:46 +0530

    Update Distinct Subsequences.cpp

--- Dynamic Programming/Distinct Subsequences.cpp
+++ Dynamic Programming/Distinct Subsequences.cpp
@@ -17,3 +17,56 @@ public:
         return solve(s, t, n, m, dp);
     }
 };
+
+// tabular
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+       int n = s.length(), m = t.length();
+       vector<vector<long long unsigned int>> dp (n+1, vector<long long unsigned int>(m+1, 0)); 
+        
+       for(int i = 0; i<=n; i++) dp[i][0] = 1; // s is empty and t is not
+
+       for (int i = 1; i <=n; ++i) {
+           for (int j = 1; j <=m; ++j) {
+               if(s[i-1] == t[j-1]) {
+                   dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+               }
+               else {
+                   dp[i][j] =dp[i-1][j];
+
+               }
+           }
+       }
+        
+       return dp[n][m];
+
+    }
+};
+
+// space optimized
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+       int n = s.length(), m = t.length(); 
+       vector<long long unsigned int> prev(m+1, 0), curr(m+1, 0);
+       for (int i = 1; i <=n; ++i) {
+           prev[0] = 1; 
+           for (int j = 1; j <=m; ++j) {
+               if(s[i-1] == t[j-1]) {
+                   curr[j] = prev[j] + prev[j-1];
+               }
+               else {
+                   curr[j] = prev[j];
+
+               }
+           }
+           prev = curr;
+       }
+        
+       return prev[m];
+
+    }
+};
