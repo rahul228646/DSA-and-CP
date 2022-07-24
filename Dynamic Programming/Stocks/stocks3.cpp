e1eb930d8fcefe7ed27920b9b296373389bf1d3b
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-24 13:53:34 +0530

    Update stocks3.cpp

--- Dynamic Programming/Stocks/stocks3.cpp
+++ Dynamic Programming/Stocks/stocks3.cpp
@@ -21,3 +21,22 @@ public:
         return solve(p, 0, 2, false);
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        vector<vector<vector<int>>> dp (n+1, vector<vector<int>>(3, vector<int>(2, 0)));
+        for(int i = n-1; i>=0; i--) {
+            for(int j = 1; j<=2; j++) {
+                dp[i][j][1] = max( dp[i+1][j-1][0] + p[i],  dp[i+1][j][1]);
+                dp[i][j][0] = max( dp[i+1][j][1] - p[i],  dp[i+1][j][0]);   
+            }
+        }
+        
+        return dp[0][2][0];
+    }
+};
+
+
