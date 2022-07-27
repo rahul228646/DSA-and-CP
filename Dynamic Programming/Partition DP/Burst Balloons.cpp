54a3d8a50a0fb9dd65ddba3ce51282258b3d70f8
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-28 00:13:50 +0530

    Update Burst Balloons.cpp

--- Dynamic Programming/Partition DP/Burst Balloons.cpp
+++ Dynamic Programming/Partition DP/Burst Balloons.cpp
@@ -28,5 +28,27 @@ public:
 //         [3, 8]    ---> burst 3  // elements of [1 5] will depend on 3 
 //     [3, 5, 8]     ---> burst 5   // elemnts of [3, 1] will depend on 5 not on [8]
 //     [3, 1, 5, 8]  ---> burst 1   // elemnts of [3] will depend on 1 not on [5, 8]
-    
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int n = nums.size();
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
+        for(int i = n-1; i>=1; i--) {
+            for(int j = i; j<n-1; j++) {
+                int mx = INT_MIN;
+                for(int k = i; k<=j; k++) {
+                    int cost = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    mx = max(mx, cost);
+                }
+                dp[i][j] = mx;
+            }
+        }
+        return dp[1][n-2];
+    }
+};
+
+
     
