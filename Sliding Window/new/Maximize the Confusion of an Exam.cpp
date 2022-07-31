cffb23dcdb9162a51f26e1316af2801d7cbf41ed
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-07-31 11:51:53 +0530

    Create  Maximize the Confusion of an Exam.cpp

---
+++ Sliding Window/new/Maximize the Confusion of an Exam.cpp
@@ -0,0 +1,45 @@
+https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
+
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        int t = 0, f = 0, n = s.length(), i = 0, j = 0, l = 0, ans = 0;
+        for(;j<n;j++) {
+            t += s[j] == 'T';
+            while(t>k) {
+               t -= s[i++] == 'T'; 
+            }
+            ans = max(ans, j-i+1);
+            f += s[j] == 'F';
+            while(f>k) {
+               f -= s[l++] == 'F'; 
+            }
+            ans = max(ans, j-l+1);
+        }
+        return ans;
+    }
+};
+
+or 
+
+
+class Solution {
+public:
+    int solve(string s, int k, char c) {
+        int x = 0, n = s.length(), i = 0, j = 0, ans = 0;
+        for(;j<n;j++) {
+            x += s[j] == c;
+            while(t>k) {
+               t -= s[i++] == c; 
+            }
+            ans = max(ans, j-i+1);
+        }
+        return ans;
+    }
+    int maxConsecutiveAnswers(string s, int k) {
+        
+        return max( solve(s, k, 'T'), solve(s, k, 'F'));
+    }
+    
+    
+};
