9f83e44ea9d30b2b0a5e5f7608e1e869661f03a5
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-03 08:41:47 +0530

    Update Count Number of Nice Subarrays.cpp

--- Array/Subarray Related/Count Number of Nice Subarrays.cpp
+++ Array/Subarray Related/Count Number of Nice Subarrays.cpp
@@ -16,3 +16,27 @@ public:
         return total;
     }
 };
+
+
+or
+    
+class Solution {
+public:
+    int slidingWindow(vector<int>& nums, int k) {
+        int i = 0, j = 0, n = nums.size(), res = 0, odd = 0;
+        for(;j<n;j++) {
+            if(nums[j]&1) odd++;
+            while(odd>k) {
+                if(nums[i++]&1) odd--;
+            }
+            res = j-i+1;
+        }
+        return res;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        // k exactly = at most k - at most k-1
+        return slidingWindow(nums, k) - slidingWindow(nums, k-1)
+    }
+    
+    
+};
