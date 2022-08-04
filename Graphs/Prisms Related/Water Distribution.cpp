97e8da505e6c1bd2e9d189ceee30563ae034c327
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-04 16:35:51 +0530

    Create Water Distribution.cpp

---
+++ Graphs/Prisms Related/Water Distribution.cpp
@@ -0,0 +1,42 @@
+int waterDist(vector<pair<int, int>> adj[], vector<int> wells, int V) {
+
+    priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
+    vector<bool> visited(V+1, false);
+    for(int i = 0 ; i<wells.size(); i++) {
+        pq.push({wells[i], i+1});
+    }
+
+    int k = 0, cost = 0;
+    while(!pq.empty() && k<V) {
+        auto [c, p] = pq.top();
+        pq.pop();
+        if(!visited[p]) {
+            visited[p] = true;
+            cost += c;
+            k++;
+            for(auto [next_p, next_cost] : adj[p]) {
+                pq.push({next_cost, next_p});
+            }
+        }
+    }
+    return cost;
+}
+int main() {
+    int V, E;
+    cin>>V>>E;
+    vector<int> wells;
+    vector<pair<int, int>> adj[V+1];
+    for(int i = 0; i<V; i++) {
+        int x;
+        cin>>x;
+        wells.push_back(x);
+    }
+    for(int i = 0; i<E; i++) {
+        int x, y, c;
+        cin>>x>>y>>c;
+        adj[x].push_back({y, c});
+    }
+
+    cout<<waterDist(adj, wells, V)<<endl;
+}
+
