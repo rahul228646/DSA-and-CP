3b83a44e47b283bbc030ff791242264a026029f9
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-16 01:06:56 +0530

    Update stone game 1.cpp

--- Stone Game/stone game 1.cpp
+++ Stone Game/stone game 1.cpp
@@ -19,3 +19,21 @@ public:
     }
 };
 
+// or 
+
+Alice clearly always wins the 2 pile game. With some effort, we can see that she always wins the 4 pile game.
+
+If Alice takes the first pile initially, she can always take the third pile. If she takes the fourth pile initially, she can always take the second pile. At least one of first + third, second + fourth is larger, so she can always win.
+
+We can extend this idea to N piles. Say the first, third, fifth, seventh, etc. piles are white, and the second, fourth, sixth, eighth, etc. piles are black. Alice can always take either all white piles or all black piles, and one of the colors must have a sum number of stones larger than the other color.
+
+Hence, Alice always wins the game.
+    
+class Solution {
+public:
+   
+    bool stoneGame(vector<int>& piles) {
+       return true;
+    }
+};
+   
