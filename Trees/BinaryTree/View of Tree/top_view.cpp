e01ea3e2faffeb3d339c638ff1f5dbb31ebac26a
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-17 14:16:41 +0530

    Update top_view.cpp

--- Trees/BinaryTree/View of Tree/top_view.cpp
+++ Trees/BinaryTree/View of Tree/top_view.cpp
@@ -2,41 +2,32 @@
 // Time : O(n) Space : O(n)
        10(0)
      /        \
-  20(-1)      30(1)   ===> 40 20 10 30 100  if hd < l(max) node goes in left (stack) if hd > r(max) node goes in right (vector) , this happens for each level
+  20(-1)      30(1)   ===> 40 20 10 30 100  
  /    \       /    \
 40(-2) 60(0) 90(0)  100(2)
          
- vector<int> topView(Node *root)
-    {
-
+vector<int> topView(Node *root) {
         //Your code here
+        unordered_map<int, int> mp;
         queue<pair<Node*, int>> q;
-        stack<int> left; // stack because we need reverse order 40 20 instead of 20 40
-        vector<int> right, ans;
-        int hd = 0, l = 0, r = 0;
+        int mx = INT_MIN, mn = INT_MAX;
         q.push({root, 0});
-        while(!q.empty()) {
-            Node *front = q.front().first;
-            int hd = q.front().second;
-            q.pop();
-            if(hd<l) {
-                left.push(front->data);
-                l = hd; 
-            }
-            else if(hd>r) {
-                right.push_back(front->data);
-                r = hd;
-            }
-            if(front->left) {
-                q.push({front->left, hd-1});
-            }
-            if(front->right) {
-                q.push({front->right, hd+1});
+        while(q.size()) {
+            int sz = q.size();
+            while(sz--) {
+                auto curr = q.front().first;
+                int hd = q.front().second;
+                q.pop();
+                mx = max(hd, mx);
+                mn = min(hd, mn);
+                if(mp.find(hd) == mp.end()) mp[hd] = curr->data;
+                if(curr->left) q.push({curr->left, hd-1});
+                if(curr->right) q.push({curr->right, hd+1});
             }
         }
-        while(!left.empty()) {ans.push_back(left.top()), left.pop();}
-        ans.push_back(root->data);
-        for(auto i : right) {ans.push_back(i);}
+        vector<int> ans;
+        for(int i = mn; i<=mx; i++) {
+            ans.push_back(mp[i]);
+        }
         return ans;
-        
-    }
+}
