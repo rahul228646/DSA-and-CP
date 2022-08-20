317950be947b116f686f34a9981d770820f4d7a6
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-21 02:37:19 +0530

    Update Get Equal Substrings Within Budget.cpp

--- Sliding Window/Get Equal Substrings Within Budget.cpp
+++ Sliding Window/Get Equal Substrings Within Budget.cpp
@@ -2,42 +2,16 @@
 class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
-        int i = 0, j = 0, temp = maxCost, ans = 0;
-        int n = s.length();
-        while(j<n) {
-            
-            if(s[j] == t[j]) {
-                ans = max(ans, j-i+1);
-                j++;
+        int cost = 0;
+        int j = 0, n = s.length(), i = 0, ans = 0;
+        for(; j<n; j++) {
+            cost += abs(s[j]-t[j]);
+            while(cost>maxCost) {
+                cost -= abs(s[i]-t[i]);
+                i++;
             }
-            else {
-                int change = abs(s[j]-t[j]);
-                while(change>temp && i<j) {
-                    temp += abs(s[i]-t[i]);
-                    i++;
-                }
-                if(change <= temp) {
-                    temp-=change;
-                    ans = max(ans, j-i+1);
-                    j++;
-                }
-                else {
-                    j++;
-                    i = j;
-                }
-            }
+            ans = max(ans, j-i+1);
         }
         return ans;
     }
 };
-
-// or
-
-int equalSubstring(string s, string t, int maxCost) {
-  auto i = 0, j = 0;
-  while (i < s.size()) {
-    maxCost -= abs(s[i] - t[i++]);
-    if (maxCost < 0) maxCost += abs(s[j] - t[j++]);
-  }
-  return i - j;
-}
