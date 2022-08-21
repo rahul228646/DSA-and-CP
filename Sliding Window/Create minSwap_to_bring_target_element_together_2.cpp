76fa1636ac4f0d574129a24fb5c22d11684d977b
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-21 14:01:53 +0530

    Update Create minSwap_to_bring_target_element_together_2.cpp

--- Sliding Window/Create minSwap_to_bring_target_element_together_2.cpp
+++ Sliding Window/Create minSwap_to_bring_target_element_together_2.cpp
@@ -35,3 +35,16 @@ public:
         return min(oneSwap, zeroSwap);
     }
 };
+
+// or
+
+int minSwaps(vector<int>& nums) {
+    int ones = count(begin(nums), end(nums), 1), n = nums.size(), res = n;
+    for (int i = 0, j = 0, cnt = 0; i < n; ++i) {
+        while (j - i < ones)
+            cnt += nums[j++ % n];
+        res = min(res, ones - cnt);
+        cnt -= nums[i];
+    }
+    return res;
+}
