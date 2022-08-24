9dffd506cbc3615c325ab6244875b15d729fe52c
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-08-25 00:08:37 +0530

    Update no_of_array_when_diff_is_given.cpp

--- Array/no_of_array_when_diff_is_given.cpp
+++ Array/no_of_array_when_diff_is_given.cpp
@@ -8,7 +8,7 @@ int numberOfArrays(vector<int>& diff, int lower, int upper) {
             ma = max(ma, a);
             mi = min(mi, a);
         }
-        return max(0L, (upper - lower) - (ma - mi) + 1);
+        return max(0L, (upper - lower) - (ma - mi + 1));
 }
 
 
@@ -21,3 +21,11 @@ Similarly, arr[3] will be X + 4, and so on.
 so arr {i.e. hidden sequence} will be [X, X + 3, X - 1, X + 4, X + 5, X + 3];
 Now, you observe here, if choose some X, it can be some maximum X + 5 & some minimum X - 1;
 Knowing these; you can easily loop over and count for all possible X's and see if X + 5 <= upper & X - 1 >= lower.
+    
+now suppose lower = 1 upper = 10 
+X-1 >= 1 && X+5<=10
+X >= 2 && X<=5
+count = 4
+(10-1) - (5-1+1)
+ 9 - 5 = 4   
+    
