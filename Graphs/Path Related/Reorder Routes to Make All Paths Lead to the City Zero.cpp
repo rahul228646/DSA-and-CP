320fa345d04e0d52a6cdfeeeb280b622ac73d02c
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2022-11-08 15:05:01 +0530

    Update Reorder Routes to Make All Paths Lead to the City Zero.cpp

--- Graphs/Path Related/Reorder Routes to Make All Paths Lead to the City Zero.cpp
+++ Graphs/Path Related/Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -22,3 +22,45 @@ public:
     }
 };
 
+
+
+class Solution {
+public:
+    int bfs(int n, vector<int>adj[], int start, vector<bool> &vis) {
+        queue<int> q;
+        q.push(start);
+        bool flag = false;
+        while(q.size()) {
+            int sz = q.size();
+            while(sz--) {
+                auto curr = q.front();
+                q.pop();
+                if(vis[curr]) {
+                    flag = true;
+                    continue;
+                }
+                vis[curr] = true;
+                for(auto i : adj[curr]) {
+                    q.push(i);
+                }
+            }
+        }
+        if(flag) return 1;
+        return 0;
+    }
+    int minReorder(int n, vector<vector<int>>& c) {
+        vector<int> adj[n];
+        vector<bool> vis(n, false);
+        int ans = 0;
+        for(auto i : c) {
+            adj[i[1]].push_back(i[0]);
+        }
+        for(int i = 0; i<n; i++) {
+            if(!vis[i]) {
+                ans += bfs(n, adj, i, vis);
+            }
+        }
+        return ans;
+    }
+};
+
