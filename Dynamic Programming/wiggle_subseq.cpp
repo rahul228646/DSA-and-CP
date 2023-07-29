d48c624208eb0a42c29a0136b7c047b86186f1c6
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2023-07-29 15:08:14 +0530

    Update wiggle_subseq.cpp

--- Dynamic Programming/wiggle_subseq.cpp
+++ Dynamic Programming/wiggle_subseq.cpp
@@ -18,3 +18,17 @@ public:
         return count;
     }
 };
+
+https://leetcode.com/problems/wiggle-subsequence/solutions/2229495/c-o-n-explained/
+
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int size=nums.size(), peak=1, valley=1;
+        for(int i=1; i<size; ++i){
+                 if(nums[i]>nums[i-1]) peak = valley + 1;
+            else if(nums[i]<nums[i-1]) valley = peak + 1;
+        }
+        return max(peak , valley );
+    }
+};
