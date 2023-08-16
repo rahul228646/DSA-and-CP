9ccf5cd9b7d521a6d437ac794afd22e8ddb00d1e
Rahul Kaushik <56363090+rahul228646@users.noreply.github.com>
2023-08-16 14:52:16 +0530

    Update next permutation.cpp

--- Array/next permutation.cpp
+++ Array/next permutation.cpp
@@ -2,14 +2,14 @@ https://leetcode.com/problems/next-permutation/submissions/
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = 0, n = nums.size(), j = 0;
-        i = n-2;
-        while(i>=0 && nums[i]>=nums[i+1]) i--;
+        int i = nums.size()-2, n = nums.size();
+        while(i>=0 && nums[i] >= nums[i+1]) i--;
         if(i>=0) {
             int j = n-1;
             while(nums[j]<=nums[i]) j--;
             swap(nums[i], nums[j]);
         }
-        reverse(nums.begin()+i+1, nums.end());
+        sort(nums.begin()+i+1, nums.end());
     }
+
 };
